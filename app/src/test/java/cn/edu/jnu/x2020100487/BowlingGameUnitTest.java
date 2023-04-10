70d60125108d66736ebfb6f168d7ffeb6ea5803d
19Zoe <3349417843@qq.com>
2023-04-10 14:59:15 +0800

    Initial commit

---
+++ app/src/test/java/cn/edu/jnu/x2020100487/BowlingGameUnitTest.java
@@ -0,0 +1,17 @@
+package cn.edu.jnu.x2020100487;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class BowlingGameUnitTest {
+    @Test
+    public void test1() {
+        assertEquals(4, 2 + 2);
+    }
+}
